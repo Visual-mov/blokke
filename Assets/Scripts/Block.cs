ff618ed21826449b65422d90312efdbf3cbcb083
Ryan <32203661+Visual-mov@users.noreply.github.com>
2020-03-01 18:18:25 -0800

    Added Blocks.
    Also added rotation and movement

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    void Start()
+    {
+        InvokeRepeating("MoveDown",1,1);
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+    void CheckInput() {
+        Vector3 move = new Vector3();
+        if(Input.GetKeyDown(KeyCode.A)) { move.x = -1;}
+        else if (Input.GetKeyDown(KeyCode.D)) { move.x = 1; }
+
+        if(Input.GetKeyDown(KeyCode.W)) {
+            // snap to bottom
+        } else if (Input.GetKeyDown(KeyCode.S)) { move.y = -1; }
+        transform.Translate(move,Space.World);
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            //transform.Rotate(transform.TransformPoint(rPoint),90,Space.Self);
+            transform.Rotate(0, 0, 90);
+        }
+    }
+    void MoveDown() {
+       transform.Translate(0, -1, 0, Space.World);
+    }
+}
